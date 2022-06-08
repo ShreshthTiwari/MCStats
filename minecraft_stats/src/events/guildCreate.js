a3c179ec419b4c3b4c1d8b4129ed33a9180a1213
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2022-06-08 08:36:01 +0530

    updates

--- minecraft_stats/src/events/guildCreate.js
+++ minecraft_stats/src/events/guildCreate.js
@@ -1,3 +1,5 @@
+let i = 0;
+
 module.exports = {
   name: 'guildCreate',
   async execute(client, embed, MessageEmbed, config, embedConfig, databaseBuilder, Permissions, messageEmojisReplacer, errorLogger, logger, guild) {
@@ -24,5 +26,27 @@ module.exports = {
     const logColor = embedConfig.successColor;
 
     await logger(client, logTitle, logText, logColor, errorLogger);
+
+    try{
+      const guildJoinImages = config.guildJoinImages;
+
+      const author  = await client.users.cache.get(config.authorID);
+  
+      embed = new MessageEmbed()
+        .setColor(embedConfig.defaultColor)
+        .setThumbnail("https://i.ibb.co/CvZ3bFp/grass.gif")
+        .setDescription(`> Thank you for inviting me to the server.
+        > Use the command \`/help\` to see all the available commands.`)
+        .setImage(guildJoinImages[i++])
+        .setFooter({text: `- ${author.tag}`});
+  
+      await guild.channels.cache.filter(ch => ch.type == "GUILD_TEXT").first().send({embeds: [embed]}).catch(error => {console.log(error)});
+
+      if(i >= guildJoinImages.length){
+        i = 0;
+      }
+    }catch(error){
+      console.log(error);
+    }
   },
 };
